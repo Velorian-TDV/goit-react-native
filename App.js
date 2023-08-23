e5cfc79b91f0b0304ed0954494c91da4332d857d
Velorian <TerleckyDima92@gmail.com>
2023-08-23 21:29:16 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log(counter)
+  }, [counter])
+
+  const handleClick = () => setCounter(prevState => prevState + 1)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles}>You click {counter} times</Text>
+      <Button title='Click me' onPress={handleClick} />
       <StatusBar style="auto" />
     </View>
   );
